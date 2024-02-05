d71a31b80b2b181effddf06edf7024e21a1382da
ASVPATM <159084542+ASVPATM@users.noreply.github.com>
2024-02-05 17:49:36 -0500

    Add files via upload
    Willamette CS structure, rest coded by me

---
+++ src/Wordle/src/edu/willamette/cs1/wordle/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * File: package-info.java
+ * -----------------------
+ * This file produces the package-level documentation for the
+ * edu.willamette.cs1.wordle package.
+ */
+
+/**
+ * This package defines the library classes supplied with the Wordle
+ * project.  Clients should not need to modify the code or understand
+ * the details of the implementation.
+ */
+
+package edu.willamette.cs1.wordle;
